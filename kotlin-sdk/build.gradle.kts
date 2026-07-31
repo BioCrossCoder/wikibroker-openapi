5dc8ddc766bcb563af72080b6730a4e199b890cf
biocrosscoder <viscaria_dignics@163.com>
2026-07-31 17:21:08 +0800

    chore: 搭建Kotlin SDK项目开发框架

---
+++ kotlin-sdk/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.4.0"
+}
+
+group = "com.wikiglobal"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ kotlin-sdk/src/main/kotlin/com/wikiglobal/wikibroker/openapi/WikiBrokerOpenApi.kt
@@ -0,0 +1,1 @@
+package com.wikiglobal.wikibroker.openapi
